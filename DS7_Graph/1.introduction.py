5f623857f4245c6ce0b5ddba4a944b24692fab78
apang <1305999617@qq.com>
2020-05-20 11:39:33 +0800

    '图处理字梯问题'

--- DS7_Graph/1.introduction.py
+++ DS7_Graph/1.introduction.py
@@ -118,6 +118,10 @@ class Graph:
     def getVertices():
         return self.vertList.keys()  
 
+    # 添加一个迭代器
+    def __iter__(self):
+        return iter(self.vertList.values())
+
 
 g = Graph()
 for i in range(6):
@@ -126,4 +130,15 @@ for i in range(6):
 # print(g.vertList)
 g.addEdge(0,1,5)
 g.addEdge(0,5,2)
+g.addEdge(1,2,4)
+g.addEdge(2,3,9)
+g.addEdge(3,4,7)
+g.addEdge(3,5,3)
+g.addEdge(4,0,1)
+g.addEdge(5,4,3)
+g.addEdge(5,2,1)
+
+for v in g:
+    for w in v.getConnections():
+        print('(%s,%s)'%(v.getId(), w.getId()))
     
